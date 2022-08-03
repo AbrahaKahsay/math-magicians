c7df99b166b1896922fd751269d348dc6ea95348
AbrahaKahsay <abrahak81@gmail.com>
2022-08-03 13:11:32 +0300

    Add App class component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,13 @@
 import React from "react";
 import Calculator from "./components/Calculator";
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (<Calculator />);
+  }
+}

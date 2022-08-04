250d4a643d8421663aaa7acd64882d03eaab92f1
AbrahaKahsay <abrahak81@gmail.com>
2022-08-04 22:00:03 +0300

    Refactor Result class component

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,20 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Result extends React.Component {
-  constructor({ name, value }) {
-    super({ name, value });
-    this.name = name;
-    this.value = value;
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
   render() {
-    return (<div id={this.name}>{this.value}</div>);
+    const {name, previous, operator, next, total } = this.props;
+
+    return (<div id={name}>
+      <span>{previous}</span>{' '}
+      <span>{operator}</span>{' '}
+      <span>{next}</span> {br}
+      <span>{total}</span>
+    </div>);
   }
 }
 
 Result.propTypes = {
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  previous: PropTypes.string.isRequired,
+  operator: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  total: PropTypes.string.isRequired,
 };
 
 export default Result;

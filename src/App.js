98854bdfba65579c9118063738176597339b4970
AbrahaKahsay <abrahak81@gmail.com>
2022-08-06 01:05:45 +0300

    Refactor App.js with function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (<Calculator />);
-  }
+function App () {
+  return (<Calculator />);
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-
 const Calculator = ()=> {
     const [state, setState] = useState({
       total: null,

c0e100d8b1b467ee9723e5705a005c56925e2662
AbrahaKahsay <abrahak81@gmail.com>
2022-08-06 00:58:54 +0300

    update the btnClick by removing the this. extension

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -26,28 +26,28 @@ const Calculator = ()=> {
         <span>{operation}</span>
         <span>{next}</span>
       </div>
-      <button type="button" onClick={this.btnClick} id="all-clear">AC</button>
-      <button type="button" onClick={this.btnClick} id="plus-minus">+/-</button>
-      <button type="button" onClick={this.btnClick} id="percent">%</button>
-      <button type="button" onClick={this.btnClick} id="divide">÷</button>
+      <button type="button" onClick={btnClick} id="all-clear">AC</button>
+      <button type="button" onClick={btnClick} id="plus-minus">+/-</button>
+      <button type="button" onClick={btnClick} id="percent">%</button>
+      <button type="button" onClick={btnClick} id="divide">÷</button>
 
-      <button type="button" onClick={this.btnClick} id="seven">7</button>
-      <button type="button" onClick={this.btnClick} id="eight">8</button>
-      <button type="button" onClick={this.btnClick} id="nine">9</button>
-      <button type="button" onClick={this.btnClick} id="multiply">x</button>
+      <button type="button" onClick={btnClick} id="seven">7</button>
+      <button type="button" onClick={btnClick} id="eight">8</button>
+      <button type="button" onClick={btnClick} id="nine">9</button>
+      <button type="button" onClick={btnClick} id="multiply">x</button>
 
-      <button type="button" onClick={this.btnClick} id="four">4</button>
-      <button type="button" onClick={this.btnClick} id="five">5</button>
-      <button type="button" onClick={this.btnClick} id="six">6</button>
-      <button type="button" onClick={this.btnClick} id="subtract">-</button>
+      <button type="button" onClick={btnClick} id="four">4</button>
+      <button type="button" onClick={btnClick} id="five">5</button>
+      <button type="button" onClick={btnClick} id="six">6</button>
+      <button type="button" onClick={btnClick} id="subtract">-</button>
 
-      <button type="button" onClick={this.btnClick} id="one">1</button>
-      <button type="button" onClick={this.btnClick} id="two">2</button>
-      <button type="button" onClick={this.btnClick} id="three">3</button>
-      <button type="button" onClick={this.btnClick} id="add">+</button>
-      <button type="button" onClick={this.btnClick} id="zero">0</button>
-      <button type="button" onClick={this.btnClick} id="dot">.</button>
-      <button type="button" onClick={this.btnClick} id="equals">=</button>
+      <button type="button" onClick={btnClick} id="one">1</button>
+      <button type="button" onClick={btnClick} id="two">2</button>
+      <button type="button" onClick={btnClick} id="three">3</button>
+      <button type="button" onClick={btnClick} id="add">+</button>
+      <button type="button" onClick={btnClick} id="zero">0</button>
+      <button type="button" onClick={btnClick} id="dot">.</button>
+      <button type="button" onClick={btnClick} id="equals">=</button>
     </div>
   );
 

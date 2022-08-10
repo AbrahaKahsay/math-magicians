a19bddca6e761735c6137f3138ea0292860db3fd
AbrahaKahsay <abrahak81@gmail.com>
2022-08-10 11:01:38 +0300

    Add router to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './components/pages/Calculator';
+import Home from './components/pages/Home';
+import Quote from './components/pages/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div id="main-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Calculator" element={<Calculator />} />
+          <Route path="/Quote" element={<Quote />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 

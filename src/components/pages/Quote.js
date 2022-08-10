6ceafc749b26b164d6fb9784ed07912f2ab0b84c
AbrahaKahsay <abrahak81@gmail.com>
2022-08-10 10:43:03 +0300

    Add Quote component

---
+++ src/components/pages/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      <h1 className="heading">Math Magicians</h1>
+      <h2 className="quote">
+        Mathematics is not about numbers, equation, computations
+        <br />
+        or algorithms. It is about understandin. - William paul Thurston
+      </h2>
+    </div>
+  );
+}
+
+export default Quote;
